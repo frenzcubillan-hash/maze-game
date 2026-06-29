6e955c84b26328d38b0b8ecd2b49ce2b87c84fda
frenzcubillan-hash <frenzcubillan@gmail.com>
2026-06-29 12:23:12 +0800

    setup production deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+
+  // IMPORTANT for GitHub Pages
+  basePath: isProd ? "/maze-game" : "",
+
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
