541a67fc0c31a81aded6a50b8c16ec5bebed8620
frenzcubillan-hash <frenzcubillan@gmail.com>
2026-06-29 11:03:43 +0800

    css

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
-  allowedDevOrigins: ['192.168.100.10']
-}
-export default nextConfig;
